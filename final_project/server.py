1160a8f6b90dbe0b18bea23ba8fc046c24612a66
rachelyeoh <yeohrachel@gmail.com>
2022-05-26 21:59:10 +1000

    remove comments in server file

--- final_project/server.py
+++ final_project/server.py
@@ -9,7 +9,6 @@ app = Flask("Web Translator")
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
     french_text = translator.englishToFrench(textToTranslate)
     return french_text
 
@@ -17,14 +16,12 @@ def englishToFrench():
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
     english_text = translator.frenchToEnglish(textToTranslate)
     return english_text
 
 
 @app.route("/")
 def renderIndexPage():
-    # Write the code to render template
     return render_template('index.html')
 
 
